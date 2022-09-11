30c69badfb0cff01df80eba78cddcee8b7ff65db
musta <mustafa774519161@gmail.com>
2022-09-11 23:48:30 +0300

    commit all project

--- app/src/main/java/com/example/usersmanagementlibraries/MainActivity.java
+++ app/src/main/java/com/example/usersmanagementlibraries/MainActivity.java
@@ -8,8 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button show, add;
+    Button show,add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
